222c9ee3ea92aaee640f5b1a7ca6e9d7959f727b
java-expert <jagjotmca@yahoo.co.in>
2016-08-20 23:26:52 +0530

    tasks

--- src/main/java/hello/Test.java
+++ src/main/java/hello/Test.java
@@ -6,10 +6,10 @@ import org.joda.time.LocalTime;
 public class Test {
   public static void main(String[] args) {
 
-    System.out.println("greeter.sayHello()");
+    System.out.println("==============================Result of test program");
     
     LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
+    System.out.println("==========================================Current local time is: " + currentTime);
   }
 }
 
@@ -17,4 +17,13 @@ public class Test {
 /*jar {
     baseName = 'gs-gradle'
     version =  '0.1.0'
-}*/
+}
+
+gradle tasks
+gradle build
+gradle run
+gradle cleano run a specific task
+gradle taskname - t
+apply plugin: 'eclipse' -->To create the Eclipse-specific descriptor files, like .projects
+gradle init --type=java-library --> to setup gradle java project
+*/
