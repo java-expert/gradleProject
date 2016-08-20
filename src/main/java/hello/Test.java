c89ff540abe2edf4aeb6184e38770e4b0947e918
java-expert <jagjotmca@yahoo.co.in>
2016-08-20 21:27:02 +0530

    second commit

--- src/main/java/hello/Test.java
+++ src/main/java/hello/Test.java
@@ -1,8 +1,20 @@
 package main.java.hello;
 
+
+import org.joda.time.LocalTime;
+
 public class Test {
   public static void main(String[] args) {
 
     System.out.println("greeter.sayHello()");
+    
+    LocalTime currentTime = new LocalTime();
+    System.out.println("The current local time is: " + currentTime);
   }
 }
+
+
+/*jar {
+    baseName = 'gs-gradle'
+    version =  '0.1.0'
+}*/
